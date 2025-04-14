ce565f9d6e2e348f6722c413307a6b9ba8838a97
Andre Weissflog <floooh@gmail.com>
2025-04-14 15:27:59 +0200

    add include

--- release.sh
+++ release.sh
@@ -8,11 +8,14 @@ git rev-parse HEAD >../dawn.ref
 cd ..
 
 rm -rf src
+rm -rf include
 mkdir -p src/tint
+mkdir -p include
 
 cp -r dawn/src/tint/api src/tint
 cp -r dawn/src/tint/lang src/tint
 cp -r dawn/src/tint/utils src/tint
+cp -r dawn/include/tint include
 
 rm -rf src/**/BUILD.*
 rm -rf src/**/*_test.cc
