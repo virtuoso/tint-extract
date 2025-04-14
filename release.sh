020bd6158055e9afef2037098c8bdb2251585ff6
Andre Weissflog <floooh@gmail.com>
2025-04-14 15:15:50 +0200

    remove BUILD files and tests

--- release.sh
+++ release.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env zsh
+# NOTE: requires zsh because of subdirectory wildstars!
 set -e
 
 git clone --depth 1 https://dawn.googlesource.com/dawn
@@ -8,3 +10,5 @@ cd ..
 rm -rf src
 mkdir -p src
 cp -r dawn/src/tint src/
+rm -rf src/**/BUILD.*
+rm -rf src/**/*_test.cc
